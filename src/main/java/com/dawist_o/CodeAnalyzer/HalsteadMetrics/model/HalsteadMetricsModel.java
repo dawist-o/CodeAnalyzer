c1ce11460f676f87b150328fee07475919eafdc0
Vladislav <vkoval2002@gmail.com>
2020-09-25 20:40:56 +0300

    Reworked Halstead Metrics and added .fxml file

--- src/main/java/com/dawist_o/CodeAnalyzer/HalsteadMetrics/model/HalsteadMetricsModel.java
+++ src/main/java/com/dawist_o/CodeAnalyzer/HalsteadMetrics/model/HalsteadMetricsModel.java
@@ -2,29 +2,15 @@ package com.dawist_o.CodeAnalyzer.HalsteadMetrics.model;
 
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.StringProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
-
+//generate auto setters getters toString and equals
 @Data
+@AllArgsConstructor
 public class HalsteadMetricsModel {
-    private IntegerProperty j;
-    private StringProperty operator;
-    private IntegerProperty j_count;
-    private IntegerProperty i;
-    private StringProperty operand;
-    private IntegerProperty i_count;
+    private HalsteadMetricsTable halsteadMetricsTable;
     private int program_dictionary;
     private int program_lenght;
     private int program_V;
-
-    public HalsteadMetricsModel(IntegerProperty j, StringProperty operator,
-                                IntegerProperty j_count, IntegerProperty i,
-                                StringProperty operand, IntegerProperty i_count) {
-        this.j = j;
-        this.operator = operator;
-        this.j_count = j_count;
-        this.i = i;
-        this.operand = operand;
-        this.i_count = i_count;
-    }
 }
