04517284ff1f7b88af3e25a0f17abf71d87d3400
Vladislav <vkoval2002@gmail.com>
2020-09-28 21:02:09 +0300

    Added MainController

---
+++ src/main/java/com/dawist_o/CodeAnalyzer/MainScreen/controller/MainController.java
@@ -0,0 +1,72 @@
+package com.dawist_o.CodeAnalyzer.MainScreen.controller;
+
+import java.io.File;
+import com.dawist_o.CodeAnalyzer.StagesController.StageController;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+
+import static com.dawist_o.CodeAnalyzer.StagesController.StageController.openHalsteadMetrics;
+import static com.dawist_o.CodeAnalyzer.StagesController.StageController.openJilbMetrics;
+
+public class MainController {
+
+    @FXML
+    private TextField filePathField;
+
+    private static final String NO_SUCH_FILE_PATH_MSG = "File with path: %s  doesn't exist or isn't .js file";
+
+    @FXML
+    void onFirstButtonClicked() {
+        String filePath = filePathField.getText();
+        if (isValidFilePath(filePath)) {
+            openHalsteadMetrics();
+        } else {
+            showInvalidFileWindow();
+        }
+    }
+
+    @FXML
+    void onSecondButtonClicked() {
+        String filePath = filePathField.getText();
+        if (isValidFilePath(filePath)) {
+            openJilbMetrics();
+        } else {
+            showInvalidFileWindow();
+        }
+    }
+
+    @FXML
+    void onBrowseButtonClicked() {
+        File fileForRead = chooseFile();
+        if (fileForRead != null)
+            filePathField.setText(fileForRead.getPath());
+    }
+
+    private File chooseFile() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("JavaScript File", "*js"));
+        return fileChooser.showOpenDialog(StageController.window);
+    }
+
+    private void showInvalidFileWindow() {
+        Alert programInfo = new Alert(Alert.AlertType.INFORMATION);
+        programInfo.setTitle("Error");
+        programInfo.setHeaderText(null);
+        programInfo.setContentText(String.format(NO_SUCH_FILE_PATH_MSG, filePathField.getText()));
+        programInfo.showAndWait();
+    }
+
+    private boolean isValidFilePath(String filePath) {
+        File file = new File(filePath);
+        if (file.exists() && filePath.endsWith(".js"))
+            return true;
+        return false;
+    }
+
+    @FXML
+    void initialize() {
+
+    }
+}
