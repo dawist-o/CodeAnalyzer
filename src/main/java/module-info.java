8b50f94671c9d59d0364d58f83220d6d8b78e42e
Vladislav <vkoval2002@gmail.com>
2020-10-02 13:20:12 +0300

    Module got back

---
+++ src/main/java/module-info.java
@@ -0,0 +1,29 @@
+module FXModule {
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires javafx.fxml;
+    requires java.desktop;
+
+    //TODO lombok(смотри HalsteadMetrics) установи плагин если не будет видеть и включи в Settings->Compiler->Annotation Processor та мгалочка
+    requires static lombok;
+    requires transitive org.mapstruct.processor;  //без него lombok выпендривается
+    requires kotlin.stdlib;
+
+    exports com.dawist_o.CodeAnalyzer.StagesController;
+    opens com.dawist_o.CodeAnalyzer.StagesController;
+
+    exports com.dawist_o.CodeAnalyzer;
+    opens com.dawist_o.CodeAnalyzer;
+
+    exports com.dawist_o.CodeAnalyzer.MainScreen.controller;
+    opens com.dawist_o.CodeAnalyzer.MainScreen.controller;
+
+    exports com.dawist_o.CodeAnalyzer.HalsteadMetrics.controller;
+    opens com.dawist_o.CodeAnalyzer.HalsteadMetrics.controller;
+
+    exports com.dawist_o.CodeAnalyzer.JilbMetrics.controller;
+    opens com.dawist_o.CodeAnalyzer.JilbMetrics.controller;
+
+    exports com.dawist_o.CodeAnalyzer.SpenAndChepinsMetrics.controller;
+    opens com.dawist_o.CodeAnalyzer.SpenAndChepinsMetrics.controller;
+}
